845294e2a0830f72ed892bb542cfb55165c95428
leprofessionnel79 <jannblomondo@gmail.com>
2021-07-28 16:54:56 +0300

    change server domin

--- lib/utilities/api_utilties.dart
+++ lib/utilities/api_utilties.dart
@@ -1,4 +1,5 @@
 String base_api = "http://192.168.1.20/newsapi/public";
+//String base_api = "http://"set server domain here"/newsapi/public";
 String all_authors_api = "/api/authors";
 String authors_api = "/api/authors";
 String all_categories_api = "/api/categories";
